cd0ec7f943f9dd412ae477275ae5e35c7be58af0
Nicola Travaglini <nicola1.travaglini@gmail.com>
2023-05-28 22:17:03 +0200

    Debugging + refactoring

--- constants.py
+++ constants.py
@@ -5,13 +5,13 @@ IMG_RESIZE_HEIGHT = 120
 
 NUM_CLASSES = 4
 
-BATCH_SIZE = 4
-NUM_EPOCHS = 5
+BATCH_SIZE = 48
+NUM_EPOCHS = 10
 LR = 0.00083
 
 OUTPUTS = {
-    0: "asso_quadri",
-    1: "dieci_cuori",
-    2: "sfondo",
-    3: "tre_fiori"
+	0: "asso_quadri",
+	1: "dieci_cuori",
+	2: "sfondo",
+	3: "tre_fiori"
 }
